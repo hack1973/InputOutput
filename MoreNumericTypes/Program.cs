fd9c0ff1d5a8f73af3fc49a282b3682b99fefd4d
Michael Hacker <hack1973@swbell.net>
2020-10-16 21:14:48 -0500

    completed morenumerictypes

---
+++ MoreNumericTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoreNumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that asks a user for the number of miles they have driven and the amount of gas they’ve consumed (in gallons),
+            //and print their miles-per-gallon.
+            Console.WriteLine("How many miles have you driven? ");
+            string userMiles = Console.ReadLine();
+            float miles = float.Parse(userMiles);
+            Console.WriteLine("How many gallons have you consumed? ");
+            string userGallons = Console.ReadLine();
+            float gallons = float.Parse(userGallons);
+            float mpg = miles / gallons;
+            Console.WriteLine("Your MPG is " + mpg);
+        }
+    }
+}
