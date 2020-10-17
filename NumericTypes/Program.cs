71398345f14ebe3c193da70aabcec0cf3c65004d
Michael Hacker <hack1973@swbell.net>
2020-10-16 19:56:55 -0500

    Add project files.

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of the rectangle?");
+            string userLength = Console.ReadLine();
+            float length = float.Parse(userLength);
+
+            Console.WriteLine("What is the width of the rectangle?");
+            string userWidth = Console.ReadLine();
+            float width = float.Parse(userWidth);
+
+            float area = length * width;
+
+            Console.WriteLine("The area of the rectangle is " + area.ToString());
+        }
+    }
+}
